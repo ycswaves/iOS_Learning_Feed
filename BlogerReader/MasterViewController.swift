3511751a072d0cc14b356c47223d23aaccafb29c
YiChenshu <ycswaves@gmail.com>
2015-05-31 15:50:08 +0800

    hook up core data

--- BlogerReader/MasterViewController.swift
+++ BlogerReader/MasterViewController.swift
@@ -18,6 +18,9 @@ class MasterViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        var context: NSManagedObjectContext = appDel.managedObjectContext!
+        
         let url = NSURL(string: "https://www.googleapis.com/blogger/v3/blogs/10861780/posts?key=AIzaSyAM-uwxaBRPK_9APEffaRM449n0iC1huU8")
         let session = NSURLSession.sharedSession()
         let task = session.dataTaskWithURL(url!, completionHandler: {
@@ -31,18 +34,32 @@ class MasterViewController: UITableViewController {
                 
                 if jsonResult.count > 0 {
                     if let items = jsonResult["items"] as? NSArray {
+                        var request = NSFetchRequest(entityName: "Posts")
+                        request.returnsObjectsAsFaults = false
+                        var results = context.executeFetchRequest(request, error: nil)!
+                        
+                        if results.count > 0 {
+                            for result in results {
+                                context.deleteObject(result as! NSManagedObject)
+                                context.save(nil)
+                            }
+                        }
                         for item in items {
                             //println(item)
                             if let title = item["title"] as? String {
-                                println(title)
-                            }
-                            
-                            if let content = item["content"] as? String {
-                                println(content)
+                                if let content = item["content"] as? String {
+                                    var newPost: NSManagedObject = NSEntityDescription.insertNewObjectForEntityForName("Posts", inManagedObjectContext: context) as! NSManagedObject
+                                    newPost.setValue(title, forKey: "title")
+                                    newPost.setValue(content, forKey: "content")
+                                    context.save(nil)
+                                    
+                                }
                             }
                         }
                     }
                 }
+                
+                self.tableView.reloadData()
             }
         })
         task.resume()
